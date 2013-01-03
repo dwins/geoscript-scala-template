97e1cbe45a0d89b908932aa78330ae4401ff7564
David Winslow <dwinslow@opengeo.org>
2013-01-03 15:59:46 -0500

    Add example build files

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "geoscript-app"
+
+organization := "org.example"
+
+version := "0.1"
+
+// libraryDependencies not used; instead we define a project dependency in
+// project/build.scala
---
+++ project/build.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+object build extends sbt.Build {
+  lazy val root = Project("root", file(".")) dependsOn(geoscript)
+  lazy val geoscript = ProjectRef(uri("git://github.com/dwins/geoscript.scala"), "library")
+}
